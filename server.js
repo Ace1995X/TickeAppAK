2f5fad12eae1374bd286d2518b2ab0c0282b15d8
AleN-2001 <alen.kotarac@gmail.com>
2024-10-27 22:49:19 +0100

    Login fix

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ passport.use(new Auth0Strategy({
   domain: process.env.AUTH0_DOMAIN,
   clientID: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  callbackURL: '/callback'
+  callbackURL: `${process.env.BASE_URL}/callback`
 }, (accessToken, refreshToken, extraParams, profile, done) => {
   return done(null, profile);
 }));
